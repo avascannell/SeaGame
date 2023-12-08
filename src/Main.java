529d43d2637a9c9740fd376a4d2077e0c79b4e86
avascannell <ava_scannell26@milton.edu>
2023-12-08 12:14:48 -0500

    created backround and first fish+forts fish move and wrap.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,108 @@
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.Graphics2D;
+import java.awt.image.BufferStrategy;
+import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Main implements Runnable {
+    final int WIDTH = 1000;
+    final int HEIGHT = 700;
+
+    //Declare the variables needed for the graphics
+    public JFrame frame;
+    public Canvas canvas;
+    public JPanel panel;
+
+    public BufferStrategy bufferStrategy;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Main ex = new Main();   //creates a new instance of the game
+        new Thread(ex).start();                 //creates a threads & starts up the code in the run( ) method
     }
+    public Image Fish1Pic;
+    public Image backround;
+
+    public fishes Fish1;
+    public Main() {
+
+
+        setUpGraphics();
+
+        //variable and objects
+        //create (construct) the objects needed for the game and load up
+        Fish1Pic= Toolkit.getDefaultToolkit().getImage("dory.png"); //load the picture
+        Fish1= new fishes(255,255);
+        backround = Toolkit.getDefaultToolkit().getImage("sea.png");
+
+
+    }// BasicGameApp()
+    public void run() {
+
+        //for the moment we will loop things forever.
+        while (true) {
+
+            moveThings();  //move all the game objects
+            render();  // paint the graphics
+            pause(20); // sleep for 10 ms
+        }
+    }
+    public void moveThings()
+    {
+        Fish1.wrap();
+    }
+    public void pause(int time ){
+        //sleep
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+
+        }
+    }
+    private void setUpGraphics() {
+        frame = new JFrame("Application Template");   //Create the program window or frame.  Names it.
+
+        panel = (JPanel) frame.getContentPane();  //sets up a JPanel which is what goes in the frame
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));  //sizes the JPanel
+        panel.setLayout(null);   //set the layout
+
+        // creates a canvas which is a blank rectangular area of the screen onto which the application can draw
+        // and trap input events (Mouse and Keyboard events)
+        canvas = new Canvas();
+        canvas.setBounds(0, 0, WIDTH, HEIGHT);
+        canvas.setIgnoreRepaint(true);
+
+        panel.add(canvas);  // adds the canvas to the panel.
+
+        // frame operations
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //makes the frame close and exit nicely
+        frame.pack();  //adjusts the frame and its contents so the sizes are at their default or larger
+        frame.setResizable(false);   //makes it so the frame cannot be resized
+        frame.setVisible(true);      //IMPORTANT!!!  if the frame is not set to visible it will not appear on the screen!
+
+        // sets up things so the screen displays images nicely.
+        canvas.createBufferStrategy(2);
+        bufferStrategy = canvas.getBufferStrategy();
+        canvas.requestFocus();
+        System.out.println("DONE graphic setup");
+
+    }
+    private void render() {
+        Graphics2D g = (Graphics2D) bufferStrategy.getDrawGraphics();
+        g.clearRect(0, 0, WIDTH, HEIGHT);
+
+        //draw the image of the astronaut
+        g.drawImage(backround, 0, 0, WIDTH, HEIGHT, null);
+        g.drawImage(Fish1Pic, Fish1.xpos, Fish1.ypos, Fish1.width, Fish1.height, null);
+
+
+
+        g.dispose();
+
+        bufferStrategy.show();
+    }
+
+
 }
--- src/fishes.java
+++ src/fishes.java
@@ -1,2 +1,108 @@
+import java.awt.*;
+
+/**
+ * Created by chales on 11/6/2017.
+ */
 public class fishes {
+
+    //VARIABLE DECLARATION SECTION
+    //Here's where you state which variables you are going to use.
+    public String name;                //holds the name of the hero
+    public int xpos;                //the x position
+    public int ypos;                //the y position
+    public int dx;                    //the speed of the hero in the x direction
+    public int dy;                    //the speed of the hero in the y direction
+    public int width;
+    public int height;
+    public boolean isAlive;
+
+    public Rectangle rec;
+    public boolean isCrashing;
+    //a boolean to denote if the hero is alive or dead.
+
+
+    // METHOD DEFINITION SECTION
+
+    // Constructor Definition
+    // A constructor builds the object when called and sets variable values.
+
+
+    //This is a SECOND constructor that takes 3 parameters.  This allows us to specify the hero's name and position when we build it.
+    // if you put in a String, an int and an int the program will use this constructor instead of the one above.
+    public fishes(int pXpos, int pYpos) {
+        xpos = pXpos;
+        ypos = pYpos;
+        dx =-1;
+        dy =-1;
+        width = 60;
+        height = 60;
+        isAlive = true;
+        rec = new Rectangle(xpos, ypos, width, height);
+
+    } // constructor
+
+    // constructor
+
+
+    //The move method.  Everytime this is run (or "called") the hero's x position and y position change by dx and dy
+    public void move() {
+
+        xpos = xpos + dx;
+        ypos = ypos + dy;
+        rec = new Rectangle(xpos, ypos, width, height);
+
+    }
+    public void bounce() {
+        if (xpos < 0) {
+            dx = -dx; //bounce off of east wall
+        }
+        // bounce off of west wall
+        if (xpos > 1000-width) {
+            dx = -dx;
+        }
+        //bounce off of north wall
+        if (ypos < 0) {
+            dy = -dy;
+        }
+        //bounce off of south wall
+        if (ypos > 700-height) {
+            dy = -dy;
+        }
+        // if (xpos < 0) {
+        // dx = -dx;
+        //}
+        xpos = xpos + dx;
+        ypos = ypos + dy;
+        rec = new Rectangle(xpos, ypos, width, height);
+
+    }
+    public void wrap() {
+        //wrap east wall
+        if(xpos < 0){
+            xpos = 1000-width;
+        }
+        if(xpos > 1000-width){
+            xpos = 0;
+        }
+        if(ypos < 0){
+            ypos= 700-height;
+        }
+        if(ypos > 700-height){
+            ypos=0;
+        }
+
+        xpos = xpos + dx;
+        ypos = ypos + dy;
+        rec = new Rectangle(xpos, ypos, width, height);
+
+    }
+
+
 }
+
+
+
+
+
+
+
